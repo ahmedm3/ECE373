02bc483f5f0fe216b76e5605660abc1974afcd55
ahmedm3 <ahmed.linkin@hotmail.com>
2016-05-03 16:04:42 -0700

    Made some variable changes - turn in

--- HW3/foo.c
+++ HW3/foo.c
@@ -19,7 +19,7 @@ int main()
 	ssize_t num_read, num_written;
 	unsigned int  my_read_str;
 	const char my_write_str[10] = "78";
-	const char led_of[10] = "15";
+	const char LED_off[10] = "15";
 
 	fd = open("/dev/part32_0", O_RDWR); //open char device
 
@@ -27,7 +27,7 @@ int main()
 	printf("current value of the LED reg is 0x%06x, (%zd bytes)\n",
 	      my_read_str, num_read); //output value
 
-	num_written = write(fd, my_write_str, sizeof(my_write_str)); // write new value to turn off LED
+	num_written = write(fd, my_write_str, sizeof(my_write_str)); // write new value to turn on LED
 	printf("successfully wrote %s (%zd bytes) to the LED reg to turn LED on\n",my_write_str, num_written);
 
 	num_read = read(fd, &my_read_str, 0);
@@ -36,7 +36,7 @@ int main()
 	sleep(2);
 
 	num_written = write(fd, led_of, sizeof(led_of));
-	printf("successfully wrote %s (%zd bytes) to the LED reg to turn LED off\n",led_of, num_written);	
+	printf("successfully wrote %s (%zd bytes) to the LED reg to turn LED off\n",LED_off, num_written); // turn off LED	
 	
 	close(fd); 
 
