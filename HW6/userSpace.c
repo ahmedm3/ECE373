be9b77d2b10f66b61e079193068f7e6240f8d8d2
Ahmed Hashim Abdulkareem <ahmed.linkin@hotmail.com>
2016-06-07 14:40:48 -0700

    corrections

--- HW6/userSpace.c
+++ HW6/userSpace.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	int fd;
 	ssize_t num_read;
 	int head, tail;
-	char  my_read[10];
+	char  myRead_str[10];
 	
 	// Open the kernel
 	fd = open("/dev/HW6", O_RDWR);
@@ -31,15 +31,15 @@ int main(int argc, char *argv[])
 	}
 
 	// Read The value syscall_val from the kernel
-	num_read = read(fd, &my_read, sizeof(int));
+	num_read = read(fd, &myRead_str, sizeof(int));
   	
 	if (num_read==-1)
 	{
 		perror("Info from kernel cannot be read! \n");
 		return -1;
 	}
-	head = (*my_read >> 16);
-	tail = *my_read & 0xFFFF;
+	head = (*myRead_str >> 16);
+	tail = *myRead_str & 0xFFFF;
 
 	printf("Head: 0x%x\n",head);
 	printf("Tail: 0x%x\n",tail);
